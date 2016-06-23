92cd3d22ad0582f85266cce4134957dbfd40a957
Hai Huynh <huynh2hai@yahoo.com>
2016-06-23 13:33:12 +0700

    Added route to show book by id

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class BooksController
 {
@@ -15,4 +16,24 @@ class BooksController
     {
         return Book::all();
     }
+
+    /**
+     * GET /books/{id}
+     *
+     * @param  int $id
+     * @return mixed
+     */
+    public function show($id)
+    {
+        try{
+            return Book::findOrFail($id);
+        } catch(ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ], 404);
+        }
+    }
+
 }
