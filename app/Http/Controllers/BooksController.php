6c805a70026d0978e2de29f5376784e55d398844
Hai Huynh <huynh2hai@yahoo.com>
2016-06-23 11:56:52 +0700

    Create books table and BooksController

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
+
 class BooksController
 {
     /**
@@ -11,9 +13,6 @@ class BooksController
      */
     public function index()
     {
-        return [
-            ['title' => 'War of the Worlds'],
-            ['title' => 'A Wrinkle in Time'],
-        ];
+        return Book::all();
     }
 }
