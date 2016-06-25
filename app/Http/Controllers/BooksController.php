64d1ee2e42d9ab58e6e0c030289e30ee78a02376
Hai Huynh <huynh2hai@yahoo.com>
2016-06-26 03:44:23 +0700

    Delete a Book

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -69,4 +69,21 @@ class BooksController
         ]);
     }
 
+    public function destroy($id)
+    {
+         try{
+            $book = Book::findOrFail($id);
+        }catch(ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                        'message' => 'Book not found'
+                    ]
+                ], 404);
+        }
+
+        $book->delete();
+
+        return response(null, 204);
+    }
+
 }
